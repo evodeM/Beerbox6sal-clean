90e6c5131efabe8d0bf22d5d29e1ab38e407b08f
evodeM <your.email@example.com>
2025-01-19 22:32:47 +0100

    Hardcode Firebase config

--- dorm-sales-app/src/firebase/config.js
+++ dorm-sales-app/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDgVwDyD1mDBl-J_C6kGvMPsgS7DkNvibg",
+  authDomain: "fridge6sal-5b0f6.firebaseapp.com",
+  projectId: "fridge6sal-5b0f6",
+  storageBucket: "fridge6sal-5b0f6.firebasestorage.app",
+  messagingSenderId: "757651497587",
+  appId: "1:757651497587:web:43bbd10fad974cad7ec006"
 };
 
 // Initialize Firebase
